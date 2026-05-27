22318d69cd87fbcf4ddb14492460b3761d07ea7d
Mike Harris <MikeMKH@users.noreply.github.com>
2026-05-27 05:51:35 -0500

    add examples from 2-1 DeMorgan's Laws extended

--- tests/chapter02/test_examples2.c
+++ tests/chapter02/test_examples2.c
@@ -85,3 +85,72 @@ Test(basics, turn_off_rightmost_contiguous_1_bits) {
   int8_t a = ((x & -x) + x) & x;
   cr_assert_eq(a, e);
 }
+
+Test(de_morgan_laws_extended, not_over_and) {
+  int8_t x = 0b10101010;
+  int8_t y = 0b11001100;
+  int8_t a = ~(x & y);
+  int8_t b = ~x | ~y;
+  cr_assert_eq(a, b);
+}
+
+Test(de_morgan_laws_extended, not_over_or) {
+  int8_t x = 0b10101010;
+  int8_t y = 0b11001100;
+  int8_t a = ~(x | y);
+  int8_t b = ~x & ~y;
+  cr_assert_eq(a, b);
+}
+
+Test(de_morgan_laws_extended, not_over_plus_1) {
+  int8_t x = 0b10101010;
+  int8_t a = ~(x + 1);
+  int8_t b = ~x - 1;
+  cr_assert_eq(a, b);
+}
+
+Test(de_morgan_laws_extended, not_over_minus_1) {
+  int8_t x = 0b10101010;
+  int8_t a = ~(x - 1);
+  int8_t b = ~x + 1;
+  cr_assert_eq(a, b);
+}
+
+Test(de_morgan_laws_extended, not_over_negation) {
+  int8_t x = 0b10101010;
+  int8_t a = ~(-x);
+  int8_t b = x - 1;
+  cr_assert_eq(a, b);
+}
+
+Test(de_morgan_laws_extended, not_over_xor) {
+  int8_t x = 0b10101010;
+  int8_t y = 0b11001100;
+  int8_t a = ~(x ^ y);
+  int8_t b = ~x ^ y;
+  cr_assert_eq(a, b);
+}
+
+Test(de_morgan_laws_extended, not_over_bitwise_equivalence) {
+  int8_t x = 0b10101010;
+  int8_t y = 0b11001100;
+  int8_t a = ~(~(x ^ y));
+  int8_t b = x ^ y;
+  cr_assert_eq(a, b);
+}
+
+Test(de_morgan_laws_extended, not_over_addition) {
+  int8_t x = 0b10101010;
+  int8_t y = 0b11001100;
+  int8_t a = ~(x + y);
+  int8_t b = ~x - y;
+  cr_assert_eq(a, b);
+}
+
+Test(de_morgan_laws_extended, not_over_subtraction) {
+  int8_t x = 0b10101010;
+  int8_t y = 0b11001100;
+  int8_t a = ~(x - y);
+  int8_t b = ~x + y;
+  cr_assert_eq(a, b);
+}
